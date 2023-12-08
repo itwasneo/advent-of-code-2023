e9bd08d9da2f8e876cbb806e8f50b1fa205b5be6
itwasneo <kaan.yildiz@protonmail.com>
2023-12-08 18:42:46 +0300

    Added some explanation for day8 solution

--- src/days/day_8.rs
+++ src/days/day_8.rs
@@ -9,6 +9,7 @@ pub fn solve() {
     let mut lines = content.lines();
     let movements = lines.next().unwrap();
 
+    // Keeping destinations from certain locations inside a HashMap
     let mut left_right_map: HashMap<&str, (&str, &str)> = HashMap::new();
     lines.skip(1).for_each(|line| {
         let (key, pair) = line.split_once(" = ").unwrap();
@@ -17,28 +18,30 @@ pub fn solve() {
     });
 
     let mut result = 0;
+
+    // Staring from "AAA"
     let mut current_location = "AAA";
+
+    // Here I used try_for_each to break from inside of a closure
     movements.chars().cycle().try_for_each(|m| {
         return match m {
             'L' => {
                 result += 1;
                 current_location = left_right_map.get(current_location).unwrap().0;
                 if current_location.eq("ZZZ") {
-                    None
-                } else {
-                    Some(())
+                    return ControlFlow::Break(m);
                 }
+                ControlFlow::Continue(())
             }
             'R' => {
                 result += 1;
                 current_location = left_right_map.get(current_location).unwrap().1;
                 if current_location.eq("ZZZ") {
-                    None
-                } else {
-                    Some(())
+                    return ControlFlow::Break(m);
                 }
+                ControlFlow::Continue(())
             }
-            _ => None,
+            _ => ControlFlow::Continue(()),
         };
     });
 
@@ -54,13 +57,24 @@ fn part_2(input: String) {
     lines.skip(1).for_each(|line| {
         let (key, pair) = line.split_once(" = ").unwrap();
         let (left, right) = pair[1..pair.len() - 1].split(", ").collect_tuple().unwrap();
+
+        // Determining starting points while traversing the rows, and keep them
+        // in a vector
         if key.ends_with("A") {
             ends_with_a.push(key);
         }
+
+        // Again keeping destionations in a HashMap
         left_right_map.insert(key, (left, right));
     });
 
+    // If you don't want to throw your pc out the window, set the type
+    // u64. lcm will be much bigger than upper limit of u32
     let mut results: Vec<u64> = vec![];
+
+    // If you check your input carefully, you will realize that you visit your
+    // final destination in a fixed size paths which by nature converts the
+    // problem into finding the lcm (least common multiplier) each path.
     for loc in ends_with_a.iter_mut() {
         let mut individual_result = 0;
         let current_location = loc;
@@ -87,6 +101,8 @@ fn part_2(input: String) {
         });
         results.push(individual_result);
     }
+
+    // I used a 3rd party library for the lcm calculation called "num"
     let result = results
         .iter()
         .cloned()
